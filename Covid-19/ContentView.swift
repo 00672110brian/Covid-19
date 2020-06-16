1c54b8cf0ecad54f09a40a33838d05c3faa2f1a2
jasper <cool.daniel1234@gmail.com>
2020-06-16 16:41:53 +0800

    Initial Commit

---
+++ Covid-19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19
+//
+//  Created by User02 on 2020/6/16.
+//  Copyright © 2020 sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
